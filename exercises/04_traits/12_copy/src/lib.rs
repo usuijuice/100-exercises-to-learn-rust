a34619c9fed29d2326337e2275010a00d2279297
usuijuice <jumpeisoccerguitar@gmail.com>
2026-01-01 22:28:36 +0900

    Implement `Add` trait for `WrappingU32` to support addition operations

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,9 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+use std::ops::Add;
+
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +11,14 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+
+    fn add(self, other: WrappingU32) -> WrappingU32 {
+        WrappingU32 {value: self.value.wrapping_add(other.value) }
     }
 }
 
