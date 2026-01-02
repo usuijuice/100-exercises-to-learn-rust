2dd5612ebd635cce3c1911d22aeb27fa5c833303
usuijuice <jumpeisoccerguitar@gmail.com>
2026-01-02 18:04:01 +0900

    Implement `easy_ticket` function to handle invalid descriptions and use a default description when necessary

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,21 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    match Ticket::new(title.clone(), description, status.clone()) {
+        Ok(ticket) => ticket,
+        Err(err) => {
+            if err != "Description cannot be empty" && err != "Description cannot be longer than 500 bytes" {
+                panic!("{}", err);
+            } else {
+                Ticket::new(
+                    title,
+                    "Description not provided".to_string(),
+                    status,
+                )
+                .unwrap()
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
