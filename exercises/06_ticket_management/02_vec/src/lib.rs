9431f3e12559c08a04efd477f9fa9175e68b80ca
usuijuice <jumpeisoccerguitar@gmail.com>
2026-01-15 20:09:30 +0900

    Implement Fibonacci function with memoization

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut fib_sequence = vec![0, 1];
+    for i in 2..=n {
+        let next_fib = fib_sequence[(i - 1) as usize] + fib_sequence[(i - 2) as usize];
+        fib_sequence.push(next_fib);
+    }
+    fib_sequence[n as usize]
 }
 
 #[cfg(test)]
