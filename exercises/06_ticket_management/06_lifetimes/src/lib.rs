3ab8e0b7118242e5f324b81a1821504a3f74863d
usuijuice <jumpeisoccerguitar@gmail.com>
2026-01-15 21:08:31 +0900

    Implement `IntoIterator` for `&TicketStore` to enable iteration over tickets

--- exercises/06_ticket_management/06_lifetimes/src/lib.rs
+++ exercises/06_ticket_management/06_lifetimes/src/lib.rs
@@ -6,6 +6,15 @@ pub struct TicketStore {
     tickets: Vec<Ticket>,
 }
 
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = std::slice::Iter<'a, Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.iter()
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ticket {
     pub title: TicketTitle,
@@ -31,7 +40,7 @@ impl TicketStore {
         self.tickets.push(ticket);
     }
 
-    pub fn iter(&self) -> std::slice::Iter<Ticket> {
+    pub fn iter(&self) -> std::slice::Iter<'_, Ticket> {
         self.tickets.iter()
     }
 }
