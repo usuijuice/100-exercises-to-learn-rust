a8ff631319015e590cbdaf336cce2b6dba7dc0ed
usuijuice <jumpeisoccerguitar@gmail.com>
2026-01-17 20:24:10 +0900

    Implement multi-threaded `sum` function to leak a vector's heap allocation and sum its halves

--- exercises/07_threads/03_leak/src/lib.rs
+++ exercises/07_threads/03_leak/src/lib.rs
@@ -6,7 +6,16 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let v = v.leak();
+    let mid = v.len() / 2;
+    let (left_half, right_half) = v.split_at(mid);
+
+    let left_thread = thread::spawn(|| left_half.iter().sum::<i32>());
+    let right_thread = thread::spawn(|| right_half.iter().sum::<i32>());
+
+    let left_sum = left_thread.join().unwrap();
+    let right_sum = right_thread.join().unwrap();
+    left_sum + right_sum
 }
 
 #[cfg(test)]
