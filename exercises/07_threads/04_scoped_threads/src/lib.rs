b044d825e1eb24695687719938eddbf1f8987e41
usuijuice <jumpeisoccerguitar@gmail.com>
2026-01-17 20:33:04 +0900

    Implement `sum` function to split a vector and compute the sum of each half using scoped threads

--- exercises/07_threads/04_scoped_threads/src/lib.rs
+++ exercises/07_threads/04_scoped_threads/src/lib.rs
@@ -3,7 +3,18 @@
 //  Don't perform any heap allocation. Don't leak any memory.
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let mid = v.len() / 2;
+    let (left, right) = v.split_at(mid);
+
+    std::thread::scope(|scope| {
+        let left_handle = scope.spawn(|| left.iter().sum::<i32>());
+        let right_handle = scope.spawn(|| right.iter().sum::<i32>());
+
+        let left_sum = left_handle.join().unwrap();
+        let right_sum = right_handle.join().unwrap();
+
+        left_sum + right_sum
+    })
 }
 
 #[cfg(test)]
